77fca715ab122c667a50654663000a0a6b65e4be
Wei-Teck Lee <arkteck@gmail.com>
2023-01-20 21:03:28 -0500

    Time: 68 ms (82.39%), Space: 43.4 MB (49.25%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.js
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.js
@@ -7,7 +7,7 @@ var restoreIpAddresses = function(s) {
   for (let i = 1; i < 4 && i < s.length; i++) {
     for (let j = i + 1; j < i + 4 && j < s.length; j++) {
       for (let k = j + 1; k < j + 4 && k < s.length; k++) {
-        let address = [s.slice(0, i), s.slice(i, j), s.slice(j, k), s.slice(k)];
+        const address = [s.slice(0, i), s.slice(i, j), s.slice(j, k), s.slice(k)];
         if (address.every(a => Number(a) < 256 && (a.length === 1 || a[0] !== '0'))) {
           ans.push(address.join('.'));
         }
