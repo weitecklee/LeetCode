899f093e1631251f73b039f3e5be38ac06761b50
Wei-Teck Lee <arkteck@gmail.com>
2023-01-27 20:47:18 -0500

    Time: 72 ms (57.78%), Space: 44.2 MB (6.67%) - LeetHub

---
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} sequence
+ * @param {string} word
+ * @return {number}
+ */
+var maxRepeating = function(sequence, word) {
+  const reg = new RegExp('(' + word + ')\\1*', 'g')
+  let max = 0;
+  for (let i = 0; i < sequence.length; i++) {
+    const matches = sequence.slice(i).match(reg);
+    if (!matches) {
+      continue;
+    }
+    for (const match of matches) {
+      max = Math.max(max, match.length / word.length)
+    }
+  }
+  return max;
+};
