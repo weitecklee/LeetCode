ef4344f670977c25f32eb39add97f7fc51d834e8
Wei-Teck Lee <arkteck@gmail.com>
2023-02-06 21:24:04 -0500

    Time: 1004 ms (62.97%), Space: 20.2 MB (51.27%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,17 @@
+class Solution:
+  def totalFruit(self, fruits: List[int]) -> int:
+    basket = collections.defaultdict(lambda :0)
+    res = 0
+    i = 0
+    curr = 0
+    for j, fruit in enumerate(fruits):
+      basket[fruit] += 1
+      curr += 1
+      while len(basket) > 2:
+        basket[fruits[i]] -= 1
+        curr -= 1
+        if basket[fruits[i]] == 0:
+          basket.pop(fruits[i])
+        i += 1
+      res = max(res, curr)
+    return res
