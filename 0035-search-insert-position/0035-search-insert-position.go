670274311105bddd0fcc7a3bc9094774bb335dc0
Wei-Teck Lee <arkteck@gmail.com>
2023-02-19 21:14:02 -0500

    Time: 2 ms (82.43%), Space: 2.9 MB (100.00%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.go
@@ -0,0 +1,16 @@
+func searchInsert(nums []int, target int) int {
+  lo := 0
+  hi := len(nums) - 1
+  for lo <= hi {
+    mid := lo + (hi - lo) / 2
+    if nums[mid] == target {
+      return mid
+    }
+    if nums[mid] < target {
+      lo = mid + 1
+    } else {
+      hi = mid - 1
+    }
+  }
+  return lo
+}
