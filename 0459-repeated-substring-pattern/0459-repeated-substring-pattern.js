fb5fd8aef8d6d4a9c77c3de19ac17589d3be7e11
Wei-Teck Lee <arkteck@gmail.com>
2023-02-23 20:59:43 -0500

    Time: 91 ms (50.62%), Space: 44.7 MB (68.73%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
@@ -3,18 +3,6 @@
  * @return {boolean}
  */
 var repeatedSubstringPattern = function(s) {
-  
-  for (let i = 0; i < s.length / 2; i++) {
-    if (s.length % (i + 1)) {
-      continue;
-    }
-    const substr = s.slice(0, i + 1);
-    const re = new RegExp('^(' + substr + ')\\1+$');
-    if (re.test(s)) {
-      return true;
-    }
-    
-  }
-  return false;
+  return /^(.+)\1+$/.test(s)
     
 };
