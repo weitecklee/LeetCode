b0a6d1b57f2dcf6d39bba8ee66be23fc746002be
Wei-Teck Lee <arkteck@gmail.com>
2023-03-28 09:36:04 -0400

    Time: 65 ms (85.71%), Space: 44.2 MB (85.71%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.ts
@@ -0,0 +1,17 @@
+function mincostTickets(days: number[], costs: number[]): number {
+  const dp: number[] = new Array(days[days.length - 1] + 1).fill(0);
+  let i = 0;
+  for (let j = 1; j < dp.length; j++) {
+    if (j === days[i]) {
+      dp[j] = Math.min(costs[0] + dp[j - 1], costs[1] + (dp[j - 7] ?? 0), costs[2] + (dp[j - 30] ?? 0));
+      i++;
+      if (i === days.length) {
+        break;
+      }
+    } else {
+      dp[j] = dp[j - 1];
+    }
+  }
+  return dp[days[days.length - 1]];
+  
+};
