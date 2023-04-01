b18274da13d4e310234ae6dea336f27f123a8cb1
Wei-Teck Lee <arkteck@gmail.com>
2023-03-31 22:05:25 -0400

    Time: 1214 ms (71.01%), Space: 20.8 MB (75.21%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.py
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.py
@@ -7,16 +7,14 @@ class Solution:
           evenSum += nums[i]
         else:
           oddSum += nums[i]
-      leftOddSum = 0
-      leftEvenSum = 0
       count = 0
       for i in range(len(nums)):
         if i % 2 > 0:
           evenSum -= nums[i]
-          leftEvenSum += nums[i - 1]
+          evenSum += nums[i - 1]
         else:
           oddSum -= nums[i]
-          leftOddSum += nums[i - 1] if i > 0 else 0
-        if leftEvenSum + evenSum == leftOddSum + oddSum:
+          oddSum += nums[i - 1] if i > 0 else 0
+        if evenSum == oddSum:
           count += 1
       return count
