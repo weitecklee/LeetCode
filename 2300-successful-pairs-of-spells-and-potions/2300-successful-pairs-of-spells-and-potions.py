a00e415b18f3b94e0b458afaab0cccd0c23d0b46
Wei-Teck Lee <weitecklee@outlook.com>
2023-09-21 11:26:16 -0400

    Time: 1171 ms (73.66%), Space: 39.3 MB (95.26%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -5,12 +5,12 @@ class Solution:
         n = len(potions)
         for spell in spells:
           if (potions[0] * spell) >= success:
-            pairs.append(len(potions))
+            pairs.append(n)
           elif (potions[-1] * spell) < success:
             pairs.append(0);
           else:
             left = 0
-            right = len(potions) - 1
+            right = n - 1
             while left < right:
               mid = left + (right - left) // 2
               check = spell * potions[mid]
@@ -18,5 +18,5 @@ class Solution:
                 left = mid + 1
               else:
                 right = mid
-            pairs.append(len(potions) - left)
+            pairs.append(n - left)
         return pairs
